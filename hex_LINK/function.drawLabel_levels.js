331431991ea6d127c3f82be32b5eecaa5599433a
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-18 16:46:02 +0500

    Add files via upload

--- hex_LINK/function.drawLabel_levels.js
+++ hex_LINK/function.drawLabel_levels.js
@@ -5,13 +5,29 @@ function drawLabel_levels() {
     ctx.textBaseline = 'middle';
     ctx.textAlign = 'center';
     ctx.fillStyle = '#323232';
-    let tw = ctx.measureText('xx/xx');
+    let tw = ctx.measureText('xx/xx').width;
+    //console.log(tw);
+    //tw = 200;
     ctx.fillText('xx/xx',
         document.body.clientWidth / 100 * 75,
         document.body.clientHeight - vmin * 3);
     // line
+    let height = tw * (Math.sqrt(3) / 2);
     ctx.beginPath();
     ctx.moveTo(
         document.body.clientWidth / 100 * 75 - tw,
         document.body.clientHeight);
+    ctx.lineTo(
+        document.body.clientWidth / 100 * 75 - tw / 2,
+        document.body.clientHeight - height);
+    ctx.lineTo(
+        document.body.clientWidth / 100 * 75 + tw / 2,
+        document.body.clientHeight - height);
+    ctx.lineTo(
+        document.body.clientWidth / 100 * 75 + tw,
+        document.body.clientHeight);
+    ctx.strokeStyle = '#323232';
+    ctx.lineWidth = vmin / 2;
+    ctx.lineJoin = 'round';
+    ctx.stroke();
 }
