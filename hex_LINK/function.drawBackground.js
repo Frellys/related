07f36c8f0542ce612a4656c0ad92ebc855c21390
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-18 17:19:48 +0500

    Add files via upload

---
+++ hex_LINK/function.drawBackground.js
@@ -0,0 +1,16 @@
+function drawBackground() {
+    ctx.fillStyle = 'whitesmoke';
+    ctx.fillRect(0, 0, core.width, core.height);
+    //let grd = ctx.createRadialGradient(
+    //    document.body.clientWidth / 2,
+    //    document.body.clientHeight / 2,
+    //    //document.body.clientHeight / 2,
+    //    0,
+    //    document.body.clientWidth / 2,
+    //    document.body.clientHeight / 2,
+    //    document.body.clientWidth);
+    //grd.addColorStop(0, 'whitesmoke');
+    //grd.addColorStop(1, '#969696');
+    //ctx.fillStyle = grd;
+    //ctx.fillRect(0, 0, core.width, core.height);
+}
