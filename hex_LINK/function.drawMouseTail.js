d8a6f3111d5641f269d3c8f27d64b1ab8bbc4235
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-20 14:28:36 +0500

    Add files via upload

--- hex_LINK/function.drawMouseTail.js
+++ hex_LINK/function.drawMouseTail.js
@@ -1,10 +1,45 @@
+// old
+//function drawMouseTail() {
+//    if (!window.mouseTail) {
+//        window.mouseTail = [];
+//        window.addEventListener('mousemove', function (e) {
+//            if (window.mouseTail.length == 5) {
+//                window.mouseTail.shift();
+//            }
+//            window.mouseTail.push({
+//                x: e.x,
+//                y: e.y,
+//                ts: new Date().getTime()
+//            });
+//        }, false);
+//    }
+//    // draw
+//    if (window.mouseTail.length != 0) {
+//        ctx.moveTo(
+//            window.mouseTail[0].x,
+//            window.mouseTail[0].y);
+//        window.mouseTail.forEach(function (el, idx) {
+//            // remove old
+//            if (new Date().getTime() - el.ts > 25) {
+//                window.mouseTail.shift();
+//            }
+//            // create line
+//            if (idx != 0) {
+//                ctx.lineTo(el.x, el.y);
+//            }
+//        });
+//        ctx.strokeStyle = '#323232';
+//        ctx.lineWidth = 2;
+//        ctx.stroke();
+//    }
+//}
+// new
 function drawMouseTail() {
     if (!window.mouseTail) {
         window.mouseTail = [];
+        window.mouseTailmaxLen = Math.min(document.body.clientWidth, document.body.clientHeight) / 100 * 10;
+        window.mouseTailcurLen = 0;
         window.addEventListener('mousemove', function (e) {
-            if (window.mouseTail.length == 5) {
-                window.mouseTail.shift();
-            }
             window.mouseTail.push({
                 x: e.x,
                 y: e.y,
@@ -14,15 +49,19 @@ function drawMouseTail() {
     }
     // draw
     if (window.mouseTail.length != 0) {
+        // remove old
+        window.mouseTail.forEach(function (el) {
+            if (new Date().getTime() - el.ts > 25) {
+                window.mouseTail.shift();
+            }
+        });
+        // reset curent length
+        window.mouseTailcurLen = 0;
+        // create line
         ctx.moveTo(
             window.mouseTail[0].x,
             window.mouseTail[0].y);
         window.mouseTail.forEach(function (el, idx) {
-            // remove old
-            if (new Date().getTime() - el.ts > 25) {
-                window.mouseTail.shift();
-            }
-            // create line
             if (idx != 0) {
                 ctx.lineTo(el.x, el.y);
             }
