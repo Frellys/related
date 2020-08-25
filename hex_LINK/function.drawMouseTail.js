a48ab42d9c666e02b30d056bbf422e184f9e2e30
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-25 15:46:07 +0500

    Add files via upload

--- hex_LINK/function.drawMouseTail.js
+++ hex_LINK/function.drawMouseTail.js
@@ -1,38 +1,3 @@
-// old
-//function drawMouseTail() {
-//    if (!window.mouseTail) {
-//        window.mouseTail = [];
-//        window.addEventListener('mousemove', function (e) {
-//            if (window.mouseTail.length == 5) {
-//                window.mouseTail.shift();
-//            }
-//            window.mouseTail.push({
-//                x: e.x,
-//                y: e.y,
-//                ts: new Date().getTime()
-//            });
-//        }, false);
-//    }
-//    // draw
-//    if (window.mouseTail.length != 0) {
-//        ctx.moveTo(
-//            window.mouseTail[0].x,
-//            window.mouseTail[0].y);
-//        window.mouseTail.forEach(function (el, idx) {
-//            // remove old
-//            if (new Date().getTime() - el.ts > 25) {
-//                window.mouseTail.shift();
-//            }
-//            // create line
-//            if (idx != 0) {
-//                ctx.lineTo(el.x, el.y);
-//            }
-//        });
-//        ctx.strokeStyle = '#323232';
-//        ctx.lineWidth = 2;
-//        ctx.stroke();
-//    }
-//}
 // new
 function drawMouseTail() {
     if (!window.mouseTail) {
