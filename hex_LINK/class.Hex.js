b3f0fb6c9c3e01e0b9cfa58f2f06d6c7f03736e8
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-28 09:09:33 +0500

    Add files via upload

--- hex_LINK/class.Hex.js
+++ hex_LINK/class.Hex.js
@@ -1,6 +1,21 @@
 class Hex {
     constructor(data) {
         this.size = data.size;
+        this.height = this.size * (Math.sqrt(3) / 2);
+        this.pos = data.pos;
+        this.selected = false;
     }
-    draw() { }
+    draw() {
+        ctx.beginPath();
+        ctx.moveTo(this.pos.x - this.size / 2, this.pos.y - this.height);
+        ctx.lineTo(this.pos.x + this.size / 2, this.pos.y - this.height);
+        ctx.lineTo(this.pos.x + this.size, this.pos.y);
+        ctx.lineTo(this.pos.x + this.size / 2, this.pos.y + this.height);
+        ctx.lineTo(this.pos.x - this.size / 2, this.pos.y + this.height);
+        ctx.lineTo(this.pos.x - this.size, this.pos.y);
+        ctx.closePath();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#323232';
+        ctx.stroke();
+    }
 }
