1b133395f50557217928c25742fcf70119a70a28
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-28 12:33:59 +0500

    Add files via upload

--- hex_LINK/class.Hex.js
+++ hex_LINK/class.Hex.js
@@ -1,21 +1,46 @@
 class Hex {
-    constructor(data) {
+    constructor(data) {;
         this.size = Math.min(document.body.clientWidth, document.body.clientHeight) / 100 * 4;
         this.height = this.size * (Math.sqrt(3) / 2);
+        this.id = data.id
         this.pos = data.pos;
+        this.offset = {
+            x: undefined,
+            y: undefined
+        };
         this.selected = false;
     }
     draw() {
-        let tx = this.selected ? core.click.x : this.pos.x;
-        let ty = this.selected ? core.click.y : this.pos.y;
+        // path
+        let tx = this.selected ? core.mouse.x - this.offset.x : this.pos.x;
+        let ty = this.selected ? core.mouse.y - this.offset.y : this.pos.y;
         ctx.beginPath();
         ctx.moveTo(tx - this.size / 2, ty - this.height);
         ctx.lineTo(tx + this.size / 2, ty - this.height);
         ctx.lineTo(tx + this.size, ty);
         ctx.lineTo(tx + this.size / 2, ty + this.height);
         ctx.lineTo(tx - this.size / 2, ty + this.height);
-        ctx.lineTo(tx - this.size, this.pos.y);
+        ctx.lineTo(tx - this.size, ty);
         ctx.closePath();
+        // select
+        if (core.mouse.pressed && ctx.isPointInPath(core.mouse.x, core.mouse.y)) {
+            if (core.selected == undefined) {
+                core.selected = this.id;
+                this.selected = true;
+                this.offset = {
+                    x: core.mouse.x - this.pos.x,
+                    y: core.mouse.y - this.pos.y
+                };
+            }
+            if (core.selected == this.id) {
+                this.selected = true;
+            }
+        }
+        else {
+            this.selected = false;
+        }
+        if (core.selected == this) console.log(true);
+        else console.log(false);
         // hover
         if (ctx.isPointInPath(core.mouse.x, core.mouse.y)) {
             ctx.shadowOffsetX = 0;
