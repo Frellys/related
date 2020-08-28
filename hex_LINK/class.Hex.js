79cb14774957540279aebe6bcd7ebf81ac1ba939
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-28 10:37:31 +0500

    Add files via upload

--- hex_LINK/class.Hex.js
+++ hex_LINK/class.Hex.js
@@ -3,12 +3,11 @@ class Hex {
         this.size = Math.min(document.body.clientWidth, document.body.clientHeight) / 100 * 4;
         this.height = this.size * (Math.sqrt(3) / 2);
         this.pos = data.pos;
-        this.hover = false;
         this.selected = false;
     }
     draw() {
-        let tx = this.pos.x;
-        let ty = this.pos.y;
+        let tx = this.selected ? core.click.x : this.pos.x;
+        let ty = this.selected ? core.click.y : this.pos.y;
         ctx.beginPath();
         ctx.moveTo(tx - this.size / 2, ty - this.height);
         ctx.lineTo(tx + this.size / 2, ty - this.height);
@@ -17,8 +16,25 @@ class Hex {
         ctx.lineTo(tx - this.size / 2, ty + this.height);
         ctx.lineTo(tx - this.size, this.pos.y);
         ctx.closePath();
-        ctx.lineWidth = 2;
+        // hover
+        if (ctx.isPointInPath(core.mouse.x, core.mouse.y)) {
+            ctx.shadowOffsetX = 0;
+            ctx.shadowOffsetY = 0;
+            ctx.shadowBlur = 2;
+            ctx.shadowColor = '#323232';
+        }
+        else {
+            ctx.shadowOffsetX = 0;
+            ctx.shadowOffsetY = 0;
+            ctx.shadowBlur = 0;
+            ctx.shadowColor = 'transparent';
+        }
+        // stroke
+        ctx.lineWidth = 4;
         ctx.strokeStyle = '#323232';
         ctx.stroke();
+        // fill
+        ctx.fillStyle = this.fillStyle ? this.fillStyle : 'whitesmoke';
+        ctx.fill();
     }
 }
