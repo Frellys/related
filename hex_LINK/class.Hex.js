4c602efcde8c38582c1c85245790e00f5442edb2
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-28 09:27:19 +0500

    Add files via upload

--- hex_LINK/class.Hex.js
+++ hex_LINK/class.Hex.js
@@ -1,18 +1,21 @@
 class Hex {
     constructor(data) {
-        this.size = data.size;
+        this.size = Math.min(document.body.clientWidth, document.body.clientHeight) / 100 * 4;
         this.height = this.size * (Math.sqrt(3) / 2);
         this.pos = data.pos;
+        this.hover = false;
         this.selected = false;
     }
     draw() {
+        let tx = this.pos.x;
+        let ty = this.pos.y;
         ctx.beginPath();
-        ctx.moveTo(this.pos.x - this.size / 2, this.pos.y - this.height);
-        ctx.lineTo(this.pos.x + this.size / 2, this.pos.y - this.height);
-        ctx.lineTo(this.pos.x + this.size, this.pos.y);
-        ctx.lineTo(this.pos.x + this.size / 2, this.pos.y + this.height);
-        ctx.lineTo(this.pos.x - this.size / 2, this.pos.y + this.height);
-        ctx.lineTo(this.pos.x - this.size, this.pos.y);
+        ctx.moveTo(tx - this.size / 2, ty - this.height);
+        ctx.lineTo(tx + this.size / 2, ty - this.height);
+        ctx.lineTo(tx + this.size, ty);
+        ctx.lineTo(tx + this.size / 2, ty + this.height);
+        ctx.lineTo(tx - this.size / 2, ty + this.height);
+        ctx.lineTo(tx - this.size, this.pos.y);
         ctx.closePath();
         ctx.lineWidth = 2;
         ctx.strokeStyle = '#323232';
