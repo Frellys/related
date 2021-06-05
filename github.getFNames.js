f9d99a3550ea3544ad57beea2f3fe533edfee340
Frellys <50921387+Frellys@users.noreply.github.com>
2021-06-05 13:21:07 +0500

    Add files via upload

--- github.getFNames.js
+++ github.getFNames.js
@@ -1,3 +1,18 @@
+// extract repo filenames
 copy(Array.from(document.querySelectorAll('div[role="grid"] > div[role="row"] > div[role="rowheader"] > span > a')).map(function (a) {
     return a.innerText.toString().replace('.js', '');
 }).join('\n'));
+
+// find repo mismatch
+document.querySelectorAll('tr').forEach(function (tr) {
+    let title = tr.querySelector('td[label="Title"]');
+    if (title) {
+        title = title.getAttribute('value');
+        if (names.includes(title)) {
+            let diff = tr.querySelector('td[label="Difficulty"] > span').innerText;
+            if (diff != 'Medium') {
+                console.log(diff + " " + title);
+            }
+        }
+    }
+});
